a14c8fd672822d14029d636e577d1e99bbfa2ea5
John Kulha <kulha.william@gmail.com>
2018-11-15 21:11:25 -0500

    Change main.js and styles.css so that memory matching cards basic functionality works, though the heights of the cards change when flipping them.

--- src/main.js
+++ src/main.js
@@ -17,29 +17,34 @@ $(document).ready(function() {
     Deck.forEach(function(card) {
         let position1 = document.getElementById(card.pos1);
         let position2 = document.getElementById(card.pos2);
-        $(position1).html(`<img class="card" src=${card.img} alt=${card.name}>`)
-        $(position2).html(`<img class="card" src=${card.img} alt=${card.name}>`)
+        $(position1).html(`
+        <span class="${card.name} card back"></span>
+        `)
+        $(position2).html(`
+        <span class="${card.name} card back"></span>
+        `)
     });
 
     let clickCounter = 0
     let clickedBoy1;
     let clickedBoy2;
 
-    $('body').on('click', (e) => {
+    $('body').on('click', '.card', (e) => {
         if (clickCounter === 0) {
             clickedBoy1 = e.target;
-            console.log(clickedBoy1.nextSibling);
-            $(clickedBoy1).addClass('flipped');
+            $(clickedBoy1).removeClass('back');
             clickCounter++;    
         } else if (clickCounter === 1) {
             clickedBoy2 = e.target;
-            $(clickedBoy2).addClass('flipped');
-            if (isEqual(clickedBoy1.alt, clickedBoy2.alt)) {
-                clickedBoy1.remove();
-                clickedBoy2.remove();
+            $(clickedBoy2).removeClass('back');
+            if (isEqual($(clickedBoy1).attr('class'), $(clickedBoy2).attr('class'))) {
+                clickedBoy1.addClass('got-it');
+                clickedBoy2.addClass('got-it');
             } else {
-                $(clickedBoy1).removeClass('flipped');
-                $(clickedBoy2).removeClass('flipped');
+                setTimeout(function () {
+                    $(clickedBoy1).addClass('back');
+                    $(clickedBoy2).addClass('back');  
+                }, 1200);
             }
             clickCounter = 0;
         }
